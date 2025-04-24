55877fef11a8f7cdaab8cf38b8f48305d9de2cb7
Mark Trosper <trosperma@icloud.com>
2025-04-24 13:58:47 -0600

    Created Step 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade? ");
+        string string_grade = Console.ReadLine();
+        int grade = int.Parse(string_grade);
+        if (grade >= 90)
+        {
+            Console.WriteLine("You got an A.");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You got an B.");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You got an C.");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You got an D.");
+        }
+        else
+        {
+            Console.WriteLine("You got an F.");
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("That's a passing grade.");
+        }
+        else if (grade < 70)
+        {
+            Console.WriteLine("That is not a passing grade.");
+        }
     }
 }
