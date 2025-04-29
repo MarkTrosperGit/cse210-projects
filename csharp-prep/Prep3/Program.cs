78b6d81e42325d2891262d58b5e4796a351098ea
Mark Trosper <trosperma@icloud.com>
2025-04-29 10:00:20 -0600

    Core Requirement One Completion

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int MagicNumber = 7;
+        int Guess = 0;
+        string GuessString;
+        while(Guess != 7)
+        {
+            Console.Write("What is the magic number? ");
+            GuessString = Console.ReadLine();
+            Guess = int.Parse(GuessString);
+            if (Guess > MagicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (Guess < MagicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
