4b552fe4fceaa6cdf72c0ba2983220bd9bc4f3a9
Mark Trosper <trosperma@icloud.com>
2025-05-01 13:29:36 -0600

    Finished the core requirements

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
-    }
+        int favoriteNum;
+        string name;
+        int squaredNum;
+
+        void DisplayWelcome()
+        {Console.WriteLine("Welcome to the program!");}
+
+        string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+            return name;
+        }
+
+        int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            favoriteNum = int.Parse(Console.ReadLine());
+            return favoriteNum;
+            }
+
+        int squareNumber(int favoriteNum)
+        {
+            squaredNum = favoriteNum * favoriteNum;
+            return squaredNum;
+        }
+
+        void DisplayResult(string name, int squaredNum)
+        {Console.WriteLine($"Brother {name}, the square of your number is {squaredNum}");}
+        DisplayWelcome();
+        name = PromptUserName();
+        favoriteNum = PromptUserNumber();
+        squaredNum = squareNumber(favoriteNum);
+        DisplayResult(name, squaredNum);
+}
 }
