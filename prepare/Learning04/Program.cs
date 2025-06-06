5aa581d776147756e42bba3256f50f8127612845
Mark Trosper <trosperma@icloud.com>
2025-06-05 22:56:37 -0600

    complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment first = new Assignment("Mark Trosper", "Math");
+        Console.WriteLine(first.GetSummary());
+        MathAssignment second = new MathAssignment("John B", "Algebra", "7.3", "20.1-23.3");
+        Console.WriteLine(second.GetSummary());
+        Console.WriteLine(second.GetHomeworkList());
+        WritingAssignment third = new WritingAssignment("Seth T", "Poetry", "Sonnet in Color");
+        Console.WriteLine(third.GetSummary());
+        Console.WriteLine(third.GetWritingInformation());
     }
 }
